17fc3f910db8132f264030336dafdc0dd5eccfc1
hiroyuki0402 <hiro1990.cloud@gmail.comcd>
2023-09-23 21:16:43 +0900

    first commit

---
+++ app/src/main/java/com/example/i_am_rich_app_android_java/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.i_am_rich_app_android_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
